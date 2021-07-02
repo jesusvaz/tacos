9988289c9176c8ec2843fece3c43d00c09576587
Jesus Vazquez <jesusvazquez@jesuss-mac-mini.lan>
2021-07-01 18:11:56 -0600

    Tacos commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+//https://images.pexels.com/photos/8448340/pexels-photo-8448340.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tacos Dorados</h1>
+      <img 
+     width="500"
+      height="600"
+      alt='Tacos Dorados'
+      src='https://images.pexels.com/photos/8448340/pexels-photo-8448340.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'></img>
+   <p>385-497-3572 place your order</p>
     </div>
   );
 }
